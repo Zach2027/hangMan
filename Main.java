0290dfd45e779925ce83b47fc2638f32d31c1f0d
Zach2027 <144712865+Zach2027@users.noreply.github.com>
2025-02-24 06:02:02 +0000

    close

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main{
     public static void main(String[] args) {
-       RunHangman game = new RunHangman();
+        PlayerTurn a = new PlayerTurn();
+        RunHangman game = new RunHangman();
        game.play();
     }
 }
--- RunHangman.java
+++ RunHangman.java
@@ -3,10 +3,19 @@ public class RunHangman {
     public void play(){
         CreatePhrase j = new CreatePhrase();        
         j.makePhrase();
-        
+        PlayerTurn a = new PlayerTurn();
+        CheckPhrase b = new CheckPhrase();
         System.out.println("Which ever player gets the full word first wins");
+        String last = "";
         while(j.getMystery()  != j.getPhrase()){
-
+            if(a.isPlayer1Turn()){
+                System.out.print("Player 1 your up");
+            }
+            else{
+                System.out.print("Player 2 your up");
+            }
+            last = a.getMystery;
+            b.CheckPhrase();
         }
     }
    
